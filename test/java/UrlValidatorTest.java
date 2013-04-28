065c00f9ce2c74f1daf32711d2b76eac8d4c8fc8
arun.singh10@wipro.com <arun.singh10@wipro.com>
2013-04-28 16:02:35 +0530

    edited url validator

--- test/java/UrlValidatorTest.java
+++ test/java/UrlValidatorTest.java
@@ -14,3 +14,7 @@ public class UrlValidatorTest extends TestCase {
 //		Thread.sleep(1000*60*10);
 //	}
 }
+cjsxhi0n cjksj jcjdki2- jiwoi ]-kjdnsjs  jjn
+ bhjdhkcfj jjejr4g   jfoakifbe  ihfekl fji
+ nfdfklfk jofle k flekpk kpfk'el kwfk 2q 23 p
+  df9eif9uefj   fjjeo dfeoj ffjefeod eofie foe
